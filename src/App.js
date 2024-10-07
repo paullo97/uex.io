6590b9991713eec6fd6de27bbaa824f3f6b00169
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-10-06 22:46:51 -0300

    include route with componente main

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import RegisterForm from "./pages/register";
 import Login from "./pages/login";
 import { AlertProvider } from "./services/alertService";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainForm from "./pages/main";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<RegisterForm />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/main" element={<MainForm />} />
         </Routes>
       </Router>
     </AlertProvider>
