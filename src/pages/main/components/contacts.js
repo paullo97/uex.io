28e3ab1e504ee97beff8aa4f10df6cc726317674
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-10-07 22:12:48 -0300

    Reference component to contact on list and include method to delete contact

--- src/pages/main/components/contacts.js
+++ src/pages/main/components/contacts.js
@@ -2,14 +2,25 @@ import { PlusOne } from "@mui/icons-material";
 import { IconButton, Tooltip } from "@mui/material";
 import StorageService from '../../../services/storageService';
 import { useEffect, useState } from "react";
+import ItemContact from "../../../components/itemContact";
+import { useAlert } from "../../../services/alertService";
 
 const ContactsList = ({ registerNewContact }) => {
+  const { showAlert } = useAlert();
+
   const [contacts, setContacts] = useState([]);
+  const [ selectedItem, setSelectedItem ] = useState(null);
 
   useEffect(() => {
     setContacts(StorageService.getContacts());
   }, [registerNewContact]);
 
+  const deleteItem = (itemIndex) => {
+    StorageService.delteContact(itemIndex);
+    showAlert('success', 'Contato Deletado com Sucesso');
+    setContacts(StorageService.getContacts());
+  }
+  
   return (
     <div style={{ width: "50%", border: "solid black 1px" }}>
       <div
@@ -30,10 +41,16 @@ const ContactsList = ({ registerNewContact }) => {
         </Tooltip>
       </div>
 
-      <div>
+      <div style={{ padding: '10px', display: 'flex', gap: '10px', flexDirection: 'column', overflowY: 'scroll', height: '89%' }}>
       {contacts.length ? (
         contacts.map((contact, index) => (
-          <p key={index}>{contact.nome} - {contact.telefone}</p>
+          <ItemContact 
+            key={index} 
+            name={contact.nome} 
+            phone={contact.telefone} 
+            onClick={() => setSelectedItem(index)} 
+            selected={selectedItem === index}
+            deleteItem={() => deleteItem(index)} />
         ))
       ) : (
         <p>Nenhum contato encontrado.</p>
